e04a3e366ae17878907f92a7956148dd25e23068
AndreyUA <infernalsteel666@gmail.com>
2023-07-16 21:37:49 +0300

    create createUser method in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(email: string, password: string): Promise<User> {
+    const user = await this.usersRepository.create({ email, password });
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
