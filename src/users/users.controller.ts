3a11bba928eedcf222edc526ee2ea701542625a1
AndreyUA <infernalsteel666@gmail.com>
2023-08-21 11:31:38 +0300

    created updater user endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
@@ -36,5 +37,13 @@ export class UsersController {
   @Delete('/:id')
   async removeUser(@Param('id') id: string): Promise<User> {
     return await this.usersService.removeUser(parseInt(id));
+  }
+
+  @Patch('/:id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() body: UpdateUserDto,
+  ): Promise<User> {
+    return await this.usersService.updateUser(parseInt(id), body);
   }
 }
