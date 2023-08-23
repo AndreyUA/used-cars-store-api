b0b5fd7570010da84a3c070b59f6dbc556599c6e
AndreyUA <infernalsteel666@gmail.com>
2023-08-23 10:13:06 +0300

    added sqlite files to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { Report } from './reports/report.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: 'db-typeorm.sqlite',
       entities: [User, Report],
       synchronize: true,
     }),
