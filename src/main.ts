fda235ba9733b65ec2dc1b4e918f8c2a3c159a3d
AndreyUA <infernalsteel666@gmail.com>
2023-08-23 10:58:23 +0300

    applied cookie-session package to app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+// TODO: resolve it!
+// ! https://docs.nestjs.com/techniques/cookies
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['randomString'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
