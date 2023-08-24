bb12cd75db73ee0f65caa4e25824d451286a810e
AndreyUA <infernalsteel666@gmail.com>
2023-08-24 14:14:12 +0300

    created first successful test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,5 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+it('can create an instance of auth service', async () => {
+  const fakeUsersService = {
+    findUsers: () => Promise.resolve([]),
+    createUser: (email: string, password: string) =>
+      Promise.resolve({
+        id: 1,
+        email,
+        password,
+      }),
+  };
+
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
+  }).compile();
+
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
+});
 
 describe('AuthService', () => {
   let service: AuthService;
