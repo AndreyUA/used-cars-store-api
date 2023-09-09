bd5f4992946ea620d6371d07a659245e40a8642f
AndreyUA <infernalsteel666@gmail.com>
2023-09-09 14:20:29 +0300

    created endpoint for report creation

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateReportDto } from './dto/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private reportRepository: Repository<Report>,
+  ) {}
+
+  async createReport(reportDto: CreateReportDto): Promise<Report> {
+    const report = this.reportRepository.create(reportDto);
+
+    await this.reportRepository.save(report);
+
+    return report;
+  }
+}
