a21c8ff7b47a102cb04c8d830726dc94de57e5ff
AndreyUA <infernalsteel666@gmail.com>
2023-09-16 14:18:26 +0300

    applied relations between user and report to reports service and controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { User } from 'src/users/user.entity';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,7 +11,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  async createReport(@Body() body: CreateReportDto) {
-    return await this.reportsService.createReport(body);
+  async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return await this.reportsService.createReport(body, user);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateReportDto } from './dto/create-report.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
@@ -11,8 +12,9 @@ export class ReportsService {
     private reportRepository: Repository<Report>,
   ) {}
 
-  async createReport(reportDto: CreateReportDto): Promise<Report> {
+  async createReport(reportDto: CreateReportDto, user: User): Promise<Report> {
     const report = this.reportRepository.create(reportDto);
+    report.user = user;
 
     await this.reportRepository.save(report);
 
